ddd3fd438391873b4f9c8e7cc93352d106698ef3
Mohamed Hassan <mohamed.hassan@check24.de>
2022-06-24 11:40:20 +0200

    Initial Commit

---
+++ TeamEventHackathon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TeamEventHackathon
+//
+//  Created by Mohamed Emad Elsayed Hassan on 24.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TeamEventHackathon/TeamEventHackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamEventHackathonApp.swift
+//  TeamEventHackathon
+//
+//  Created by Mohamed Emad Elsayed Hassan on 24.06.22.
+//
+
+import SwiftUI
+
+@main
+struct TeamEventHackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
